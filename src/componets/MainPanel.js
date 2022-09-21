c37805a5990a9e8b69ce8c8d541bde5435aecd17
mefercs <mivki890@gmail.com>
2022-09-20 19:55:53 -0500

    new colors and html word tag

--- src/componets/MainPanel.js
+++ src/componets/MainPanel.js
@@ -1,15 +1,19 @@
 import VolumenSlider from "./VolumeSlider";
 import {useState} from 'react'
 import Buttons from './Buttons'
-
+import Word from './Word'
 
 
 const MainPanel = () =>{ 
 
   const [volume, setVolume] = useState(1)
+  const [word, setWord] = useState("")
 
   return <div className="main-panel">
-    <VolumenSlider setVolume={setVolume}/>
+    <div className="volume-word">
+      <VolumenSlider setVolume={setVolume}/>
+      <Word/>
+    </div>
     <Buttons volume={volume}/>
 
   </div>
---
+++ src/componets/Word.js
@@ -0,0 +1,9 @@
+
+
+const Word = (props) =>{ 
+  return <div className="word">
+    here is the word
+  </div>
+}
+
+export default Word
